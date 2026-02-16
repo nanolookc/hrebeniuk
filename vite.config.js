c3173fce0d42cf530ed0c1afb2e469a586f02eea
Dima Hrebeniuk <nanolookg@gmail.com>
2026-02-16 14:21:26 +0200

    initial

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  root: 'src',
+  plugins: [tailwindcss()],
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+})
